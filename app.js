ed55b790eb0f42e8c4d84b1b3dd222d5e4259bfd
Ramasenthilmurugan <152271299+Ramasenthilmurugan@users.noreply.github.com>
2024-05-02 13:19:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import ResponseDisplay from './ResponseDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponseDisplay />
+    </div>
+  );
+}
+
+export default App;
